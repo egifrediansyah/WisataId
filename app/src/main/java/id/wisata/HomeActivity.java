80cc8363e35fb3c3ddd9ee94e51f9064b2da9167
Fajar Jr <fajarjunyor13@gmail.com>
2019-06-14 16:47:19 +0700

    login undone

--- app/src/main/java/id/wisata/HomeActivity.java
+++ app/src/main/java/id/wisata/HomeActivity.java
@@ -1,4 +1,44 @@
 package id.wisata;
 
-public class HomeActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity {
+
+    List<Place> lstPlace;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        lstPlace = new ArrayList<>();
+        lstPlace.add(new Place("Gunung Api", R.drawable.mountain_example, "kedoya utara", 4.5f));
+        lstPlace.add(new Place("Gunung Api", R.drawable.mountain_example, "kedoya utara", 4.5f));
+        lstPlace.add(new Place("Gunung Api", R.drawable.mountain_example, "kedoya utara", 4.5f));
+        lstPlace.add(new Place("Gunung Api", R.drawable.mountain_example, "kedoya utara", 4.5f));
+        lstPlace.add(new Place("Gunung Api", R.drawable.mountain_example, "kedoya utara", 4.5f));
+        lstPlace.add(new Place("Gunung Api", R.drawable.mountain_example, "kedoya utara", 4.5f));
+        lstPlace.add(new Place("Gunung Api", R.drawable.mountain_example, "kedoya utara", 4.5f));
+        lstPlace.add(new Place("Gunung Api", R.drawable.mountain_example, "kedoya utara", 4.5f));
+        lstPlace.add(new Place("Gunung Api", R.drawable.mountain_example, "kedoya utara", 4.5f));
+        lstPlace.add(new Place("Gunung Api", R.drawable.mountain_example, "kedoya utara", 4.5f));
+        lstPlace.add(new Place("Gunung Api", R.drawable.mountain_example, "kedoya utara", 4.5f));
+        lstPlace.add(new Place("Gunung Api", R.drawable.mountain_example, "kedoya utara", 4.5f));
+
+
+
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview_id);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, lstPlace);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        recyclerView.setAdapter(adapter);
+
+    }
 }
