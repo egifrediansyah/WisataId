b4ea0a67b50ae13b237a15d90f1f9152de9d524d
egifrediansyah <panjidwinanto4@gmail.com>
2019-06-20 11:22:08 +0700

    desain

---
+++ app/src/main/java/id/wisata/RecyclerViewAdapter.kt
@@ -0,0 +1,47 @@
+package id.wisata
+
+import android.content.Context
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+class RecyclerViewAdapter(private val ctx: Context, private val data: List<Place>) :
+    RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): MyViewHolder {
+
+        val view: View
+        val inflater = LayoutInflater.from(ctx)
+        view = inflater.inflate(R.layout.place_item_card_view, viewGroup, false)
+
+        return MyViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(myViewHolder: MyViewHolder, i: Int) {
+
+        myViewHolder.placeName.text = data[i].name
+        myViewHolder.placePicture.setImageResource(data[i].imageUrl)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        internal var placeName: TextView
+        internal var placePicture: ImageView
+
+        init {
+
+            placeName = itemView.findViewById<View>(R.id.placeName) as TextView
+            placePicture = itemView.findViewById<View>(R.id.placePicture) as ImageView
+        }
+    }
+}
---
+++ app/src/main/java/id/wisata/view/Login.kt
@@ -0,0 +1,106 @@
+package id.wisata.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import id.wisata.R
+import kotlinx.android.synthetic.main.sign_in_layout.*
+
+
+class Login  : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 1
+
+    private lateinit var mGoogleSignClient : GoogleSignInClient
+
+    private lateinit var mAuth : FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_in_layout)
+
+        //inisialisasi mAuth
+        mAuth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(
+            GoogleSignInOptions.DEFAULT_SIGN_IN
+        ).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+
+        mGoogleSignClient = GoogleSignIn.getClient(this, gso)
+       sign_in_button.setOnClickListener{
+            signIn()
+        }
+    }
+
+
+
+    private fun  signIn(){
+        val signInIntent = mGoogleSignClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    private fun firebaseAuthWithGoogle(
+        acct: GoogleSignInAccount
+    ){
+        Log.d("LOGIN", "firebaseAuth"+ acct.id!! )
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this){
+                task ->
+            if (task.isSuccessful){
+                Log.d("LOGIN", "Sign In SUKSES")
+                val user = mAuth.currentUser
+                updateUI(user)
+
+            }else{
+                Log.w("LOGIN", "Sign In Error", task.exception)
+                Toast.makeText(this, "Sign In Failure", Toast.LENGTH_SHORT).show()
+                updateUI(null)
+            }
+        }
+    }
+
+    fun updateUI(user : FirebaseUser?){
+        if(user != null){
+            Toast.makeText(this, "Hello"+
+                    "${user.displayName}",
+                Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this@Login, Home::class.java))
+            finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+
+        if(requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(
+                    ApiException::class.java
+                )
+                firebaseAuthWithGoogle(account!!)
+            }catch (e: ApiException){
+                Log.w("LOGIN", "login failed", e)
+            }
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = mAuth.currentUser
+        updateUI(currentUser)
+    }
+}
