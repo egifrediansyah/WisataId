96476c2ec6d159a17bbed7d1de645d3ddb04d865
Fajar Jr <fajarjunyor13@gmail.com>
2019-07-09 14:54:59 +0700

    placedetailviewmodel&navgraph

--- app/src/main/java/id/wisata/view/ui/PlaceDetailActivity.kt
+++ app/src/main/java/id/wisata/view/ui/PlaceDetailActivity.kt
@@ -1,33 +1,39 @@
 package id.wisata.view.ui
 
 import android.arch.lifecycle.ViewModelProviders
+import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 import id.wisata.R
+import id.wisata.data.model.Place
 import id.wisata.viewModel.PlaceDetailViewModel
 import kotlinx.android.synthetic.main.item_place_detail.*
 
-class PlaceDetailActivity : AppCompatActivity(){
+class PlaceDetailActivity : AppCompatActivity() {
 
-    private lateinit var viewModel : PlaceDetailViewModel
+    private lateinit var viewModel: PlaceDetailViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-                setContentView(R.layout.item_place_detail)
+        setContentView(R.layout.item_place_detail)
+        viewModel = ViewModelProviders.of(this).get(PlaceDetailViewModel::class.java)
         btn_back.setOnClickListener {
-           onBackPressed()
-            getPlaceDetail()
-
+            onBackPressed()
+            val PlaceId = 0
+            PlaceId?.let {
+                viewModel.getPlaceDetails(PlaceId).observe(this, Observer { PlaceDetails ->
+                    if (PlaceDetails != null) {
+                        populatePlaceDetails(PlaceDetails)
+                    }
+                })
+            }
         }
+
+
     }
 
-    private fun getPlaceDetail(){
-//        val PlaceId = arguments?.getInt(getString(R.string.place_id))
-//        PlaceId?.let{
-//            viewModel.getBookDetails(BookId).observe(this, Observer { BookDetails ->
-//                populateBookDetails(BookDetails)
-//            })
+    private fun populatePlaceDetails(place: Place) {
+        placePicture.setImageDrawable(resources.getDrawable(place.imageUrl))
+        ratingbar.rating = place?.rating!!
     }
-
-
 }
